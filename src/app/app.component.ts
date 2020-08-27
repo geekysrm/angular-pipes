a06721939400fbc743a93a9b3eba4cde2c7d8601
geekysrm <soumyacool2012@gmail.com>
2020-08-28 01:35:59 +0530

    add decimal pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,11 @@ export class AppComponent {
   name: string;
   date: string;
   amount: number;
+  height: number;
+
+  onHeightChange(value: string) {
+    this.height = Number(value);
+  }
 
   onNameChange(value: string) {
     this.name = value;
