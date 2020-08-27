d066354fc4b343239e52f80bdda35afb624304b4
geekysrm <soumyacool2012@gmail.com>
2020-08-28 01:26:59 +0530

    add currency pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   name: string;
   date: string;
+  amount: number;
 
   onNameChange(value: string) {
     this.name = value;
   }
   onDateChange(value: string) {
     this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = Number(value);
   }
 }
