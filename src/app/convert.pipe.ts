53a6e3fc7a43c3291f95dd0216892dd556a0d41f
geekysrm <soumyacool2012@gmail.com>
2020-08-28 02:35:03 +0530

    add custom pipe

--- src/app/convert.pipe.ts
+++ src/app/convert.pipe.ts
@@ -4,11 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'convert',
 })
 export class ConvertPipe implements PipeTransform {
-  transform(value: number, ...args: unknown[]): unknown {
+  transform(value: number, targetUnits: string): unknown {
+    // ...args: unknown[]: args is an array of all the arguments being passed from the template
+
     if (!value) {
       return ''; //to remove NaN when value(miles) is undefined at the start
     }
 
-    return value * 1.609;
+    switch (targetUnits) {
+      case 'm':
+        return value * 1.6093 * 1000;
+      case 'km':
+        return value * 1.6093;
+      default:
+        throw new Error('Target Unit not supported');
+    }
   }
 }
